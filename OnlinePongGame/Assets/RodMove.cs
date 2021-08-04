2160af48f40c4e37a241c4471e0d7515ea4cc9b1
EmirGaziKopar <75802906+EmirGaziKopar@users.noreply.github.com>
2021-08-04 16:07:58 +0300

    1.5

--- OnlinePongGame/Assets/RodMove.cs
+++ OnlinePongGame/Assets/RodMove.cs
@@ -20,7 +20,7 @@ public class RodMove : MonoBehaviour
             if (PhotonNetwork.IsMasterClient)
             {
                 transform.position = new Vector3(7, 0, -6.5f);
-                InvokeRepeating("oyuncukontrol",0,0.5f);
+                InvokeRepeating("playerControl",0,0.5f); //her yarým saniyede bir çalýþmasý gereken bir fonksiyon tanýmladýk
 
             }
             else if (!PhotonNetwork.IsMasterClient)
@@ -28,6 +28,44 @@ public class RodMove : MonoBehaviour
                 transform.position = new Vector3(-7, 0, -6.5f);
             }
         }
+    }
+
+
+    void playerControl()
+    {
+        if(PhotonNetwork.PlayerList.Length == 2)
+        {
+            pw.RPC("clearText", RpcTarget.All, null);
+            GameObject.Find("Ball").GetComponent<PhotonView>().RPC("startToGame", RpcTarget.All, null); //top'un içersindeki startToGame fonksiyonuna ulaþarak topu harekete geçirdik
+            CancelInvoke("playerControl"); //her yarým saniyede bir çalýþmaya ayarlý fonksiyonda böylelikle durduruldu.
+            //player control oyunun baþýnda ancak bir kez çalýþabelecek 2 oyuncu þartý saðlandýktan hemen sonra CancelInvoke edilecek.
+        }
+    }
+
+
+
+
+    [PunRPC]
+    public void clearText()
+    {
+        InformationText.text = null;
+    }
+
+
+    private void Update()
+    {
+        if (pw.IsMine) //bunu yapmazsam kendi ekranýmda tüm tahtalarýn hareket ettiðini görürüm
+        {
+            rodMove();
+        }
+    }
+
+
+
+    void rodMove()
+    {
+        float vertical = Input.GetAxis("Mouse Y") * Time.deltaTime * 20;
+        transform.Translate(0, vertical, 0);
     }
 
 
