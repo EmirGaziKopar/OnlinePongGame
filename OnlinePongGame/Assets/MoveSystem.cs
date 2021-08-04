dd695b0365c408ac3f175f4ac0af5ae64002fc5b
EmirGaziKopar <75802906+EmirGaziKopar@users.noreply.github.com>
2021-08-04 14:19:01 +0300

    1.2

--- OnlinePongGame/Assets/MoveSystem.cs
+++ OnlinePongGame/Assets/MoveSystem.cs
@@ -21,4 +21,46 @@ public class MoveSystem : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (pw.IsMine == true)
+        {
+
+            move();
+
+        }
+    }
+
+
+    private void move()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 30.0f; //her saniye 30 birim hareket gücü saðlar
+        float y = Input.GetAxis("Vertical") * Time.deltaTime * 30.0f; //her saniye 30 birim hareket gücü saðlar
+
+        transform.Translate(x, 0, y); //Belirlenen pozisyona geçiþi saðlar 
+        
+    }
+
+
+
+    void ball()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(transform.position , transform.forward , out hit , 100.0f)) //ateþ 
+            {
+                hit.collider.gameObject.GetComponent<PhotonView>().RPC("yokol", RpcTarget.All, null);
+            }
+        }
+    }
+
+
+    [PunRPC]
+    public void yokol()
+    {
+        PhotonNetwork.Destroy(gameObject);//biz ateþ ettiðimizde bu herhangi bir nesneye çarptðýnda onun yok ol fonksiyonuna eriþecek ve nesnemiz silinecek (sunucuda da yani tüm bilgisayarlarda)
+    }
+
 }
